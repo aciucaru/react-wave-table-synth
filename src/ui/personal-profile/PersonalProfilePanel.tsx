7c0bb90f21be99cf33d8a3095908194cee608e21
andrei c <andr@home.com>
2023-07-24 22:38:27 +0300

    fixed personal pages wrong links

--- src/ui/personal-profile/PersonalProfilePanel.tsx
+++ src/ui/personal-profile/PersonalProfilePanel.tsx
@@ -5,10 +5,11 @@ export function PersonalProfilePanel(props: any): JSX.Element
 {
     return (
         <div className={styles.mainContainer}>
-            <a href="https://github.com/aciucaru/" target="_blank"
+            <a href="https://www.linkedin.com/in/andrei-ciucaru-411710284/" target="_blank"
             className={styles.bgImage + ' ' + styles.linkedInLink}></a>
 
-            <a href="https://www.linkedin.com/in/andrei-ciucaru-411710284/" target="_blank"
+
+            <a href="https://github.com/aciucaru/" target="_blank"
             className={styles.bgImage + ' ' + styles.gitHubLink}></a>
         </div>
     );
